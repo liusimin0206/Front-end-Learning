dd648fff47b739d441d241917c614c9eca995102
刘思民 <liusm@deepblueai.com>
2020-10-28 09:56:43 +0800

    更一下日报

---
+++ simon工作汇报/temp.js
@@ -0,0 +1,32 @@
+const documentPages = {
+    2: 'face_detection',
+    3: 'face_recognition',
+    4: 'head_pose_recognition',
+    5: 'face_emotion_recognition',
+    6: 'mask-recognition',
+    // 7: 'garbage_classification', //垃圾分类
+    8: 'safety-helmet-recognition',
+    9: 'license-plate-recognition',
+    10: 'vehicle-recognition',
+    11: 'search-by-Image',
+    14: 'pedestrian_detection',
+    // 15: 'prohibited_vehicle_detection',
+    // 16: 'vehicle_fire_detection',
+    17: 'ID_card_identification',
+    18: 'potato-DAIP-identification',
+    19: 'apple-DAIP-identification',
+    20: 'gender_and_age_prediction',
+    21: 'passport_identification',
+    81: 'face_verification',
+    // 71: 'forestAnimal_recognition',
+    // 23: 'bioassay_recognition', //活体检测
+    24: 'helmet-recognition',
+    72: 'OCR_outpatientInvoice',
+    73: 'OCR_inpatientInvoice',
+    74: 'OCR_electronicInvoice',
+    75: 'OCR_medicalinsurancecard',
+    76: 'OCR_bankCard',
+    77: 'OCR_costList',
+    78: 'OCR_leaveHospitalReport',
+    79: 'OCR_invoice'
+  }
