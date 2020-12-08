3aab5a2980cbbcf2fd92a96ff124701b0ddeab6d
刘思民 <liusm@deepblueai.com>
2020-12-08 16:45:45 +0800

    汇报

---
+++ simon工作汇报/temp1.js
@@ -0,0 +1,376 @@
+/*
+ * @Author: liusimin
+ * @Date: 2020-12-08 16:14:08
+ * @LastEditors: your name
+ * @LastEditTime: 2020-12-08 16:28:25
+ * @Description: file content
+ */
+var json = {
+  "code": 200,
+  "msg": "success",
+  "data": {
+    "text": "近年来，山东省海洋经济实力不断提升，2019年全省海洋生产总值1.46万亿元，居全国第二位，占全省GDP的20.5%，占全国海洋生产总值的16.3%。国家级海洋经济示范区数量及海洋生物医药、海水淡化与综合利用等新兴产业规模居全国首位。",
+    "entities": [
+      {
+        "start": 0,
+        "end": 2,
+        "item": "近年来",
+        "label": "l"
+      },
+      {
+        "start": 3,
+        "end": 3,
+        "item": "，",
+        "label": "w"
+      },
+      {
+        "start": 4,
+        "end": 6,
+        "item": "山东省",
+        "label": "ns"
+      },
+      {
+        "start": 7,
+        "end": 8,
+        "item": "海洋",
+        "label": "n"
+      },
+      {
+        "start": 9,
+        "end": 10,
+        "item": "经济",
+        "label": "n"
+      },
+      {
+        "start": 11,
+        "end": 12,
+        "item": "实力",
+        "label": "n"
+      },
+      {
+        "start": 13,
+        "end": 14,
+        "item": "不断",
+        "label": "d"
+      },
+      {
+        "start": 15,
+        "end": 16,
+        "item": "提升",
+        "label": "vt"
+      },
+      {
+        "start": 17,
+        "end": 17,
+        "item": "，",
+        "label": "w"
+      },
+      {
+        "start": 18,
+        "end": 22,
+        "item": "2019年",
+        "label": "t"
+      },
+      {
+        "start": 23,
+        "end": 24,
+        "item": "全省",
+        "label": "n"
+      },
+      {
+        "start": 25,
+        "end": 26,
+        "item": "海洋",
+        "label": "n"
+      },
+      {
+        "start": 27,
+        "end": 28,
+        "item": "生产",
+        "label": "vn"
+      },
+      {
+        "start": 29,
+        "end": 30,
+        "item": "总值",
+        "label": "n"
+      },
+      {
+        "start": 31,
+        "end": 36,
+        "item": "1.46万亿",
+        "label": "m"
+      },
+      {
+        "start": 37,
+        "end": 37,
+        "item": "元",
+        "label": "qd"
+      },
+      {
+        "start": 38,
+        "end": 38,
+        "item": "，",
+        "label": "w"
+      },
+      {
+        "start": 39,
+        "end": 39,
+        "item": "居",
+        "label": "vt"
+      },
+      {
+        "start": 40,
+        "end": 41,
+        "item": "全国",
+        "label": "n"
+      },
+      {
+        "start": 42,
+        "end": 43,
+        "item": "第二",
+        "label": "m"
+      },
+      {
+        "start": 44,
+        "end": 44,
+        "item": "位",
+        "label": "Ng"
+      },
+      {
+        "start": 45,
+        "end": 45,
+        "item": "，",
+        "label": "w"
+      },
+      {
+        "start": 46,
+        "end": 46,
+        "item": "占",
+        "label": "vt"
+      },
+      {
+        "start": 47,
+        "end": 48,
+        "item": "全省",
+        "label": "n"
+      },
+      {
+        "start": 49,
+        "end": 51,
+        "item": "GDP",
+        "label": "nx"
+      },
+      {
+        "start": 52,
+        "end": 52,
+        "item": "的",
+        "label": "u"
+      },
+      {
+        "start": 53,
+        "end": 57,
+        "item": "20.5%",
+        "label": "m"
+      },
+      {
+        "start": 58,
+        "end": 58,
+        "item": "，",
+        "label": "w"
+      },
+      {
+        "start": 59,
+        "end": 59,
+        "item": "占",
+        "label": "vt"
+      },
+      {
+        "start": 60,
+        "end": 61,
+        "item": "全国",
+        "label": "n"
+      },
+      {
+        "start": 62,
+        "end": 63,
+        "item": "海洋",
+        "label": "n"
+      },
+      {
+        "start": 64,
+        "end": 65,
+        "item": "生产",
+        "label": "vn"
+      },
+      {
+        "start": 66,
+        "end": 67,
+        "item": "总值",
+        "label": "n"
+      },
+      {
+        "start": 68,
+        "end": 68,
+        "item": "的",
+        "label": "u"
+      },
+      {
+        "start": 69,
+        "end": 73,
+        "item": "16.3%",
+        "label": "m"
+      },
+      {
+        "start": 74,
+        "end": 74,
+        "item": "。",
+        "label": "w"
+      },
+      {
+        "start": 75,
+        "end": 77,
+        "item": "国家级",
+        "label": "b"
+      },
+      {
+        "start": 78,
+        "end": 79,
+        "item": "海洋",
+        "label": "n"
+      },
+      {
+        "start": 80,
+        "end": 81,
+        "item": "经济",
+        "label": "n"
+      },
+      {
+        "start": 82,
+        "end": 84,
+        "item": "示范区",
+        "label": "n"
+      },
+      {
+        "start": 85,
+        "end": 86,
+        "item": "数量",
+        "label": "n"
+      },
+      {
+        "start": 87,
+        "end": 87,
+        "item": "及",
+        "label": "c"
+      },
+      {
+        "start": 88,
+        "end": 89,
+        "item": "海洋",
+        "label": "n"
+      },
+      {
+        "start": 90,
+        "end": 91,
+        "item": "生物",
+        "label": "n"
+      },
+      {
+        "start": 92,
+        "end": 93,
+        "item": "医药",
+        "label": "n"
+      },
+      {
+        "start": 94,
+        "end": 94,
+        "item": "、",
+        "label": "w"
+      },
+      {
+        "start": 95,
+        "end": 96,
+        "item": "海水",
+        "label": "n"
+      },
+      {
+        "start": 97,
+        "end": 98,
+        "item": "淡化",
+        "label": "vt"
+      },
+      {
+        "start": 99,
+        "end": 99,
+        "item": "与",
+        "label": "c"
+      },
+      {
+        "start": 100,
+        "end": 101,
+        "item": "综合",
+        "label": "vt"
+      },
+      {
+        "start": 102,
+        "end": 103,
+        "item": "利用",
+        "label": "vt"
+      },
+      {
+        "start": 104,
+        "end": 104,
+        "item": "等",
+        "label": "u"
+      },
+      {
+        "start": 105,
+        "end": 106,
+        "item": "新兴",
+        "label": "b"
+      },
+      {
+        "start": 107,
+        "end": 108,
+        "item": "产业",
+        "label": "n"
+      },
+      {
+        "start": 109,
+        "end": 110,
+        "item": "规模",
+        "label": "n"
+      },
+      {
+        "start": 111,
+        "end": 111,
+        "item": "居",
+        "label": "vt"
+      },
+      {
+        "start": 112,
+        "end": 113,
+        "item": "全国",
+        "label": "n"
+      },
+      {
+        "start": 114,
+        "end": 114,
+        "item": "首",
+        "label": "m"
+      },
+      {
+        "start": 115,
+        "end": 115,
+        "item": "位",
+        "label": "Ng"
+      },
+      {
+        "start": 116,
+        "end": 116,
+        "item": "。",
+        "label": "w"
+      }
+    ]
+  }
+}
