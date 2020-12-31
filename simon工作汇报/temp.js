0030f9b383f541e72a985234a6e4159525465fc8
刘思民 <liusm@deepblueai.com>
2020-12-31 17:49:59 +0800

    日报

--- simon工作汇报/temp.js
+++ simon工作汇报/temp.js
@@ -2,13 +2,11 @@
  * @Author: liusimin
  * @Date: 2020-10-23 18:08:57
  * @LastEditors: your name
- * @LastEditTime: 2020-12-28 14:49:47
+ * @LastEditTime: 2020-12-31 16:40:40
  * @Description: file content
  */
-for (let i = 0; i < 100000000; i++) {
-  if (Math.random() > Math.random()) {
-    console.log(111);
-    break;
-  }
+function jiaxiahua(str) {
+  str=str.toLowerCase()
+  return str.replace(/ /g, '_')
 }
-
+console.log(jiaxiahua('Intelligent Security'));
