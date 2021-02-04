8e4ff00bed56f410a174905a48848ca26973f99e
Liusimin <liusm@deepblueai.com>
2021-02-04 18:10:10 +0800

    汇报

--- simon工作汇报/temp.js
+++ simon工作汇报/temp.js
@@ -5,8 +5,7 @@
  * @LastEditTime: 2020-12-31 16:40:40
  * @Description: file content
  */
-function jiaxiahua(str) {
-  str=str.toLowerCase()
-  return str.replace(/ /g, '_')
-}
-console.log(jiaxiahua('Intelligent Security'));
+var arr=document.querySelectorAll('img')
+for(let i=0;i<arr.length;i++){console.log(arr[i].src)}
+
+
