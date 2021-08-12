94fc89c9806446c7bec140bccae151cccf1a80d9
Liusimin <liusm@deepblueai.com>
2021-08-12 09:43:01 +0800

    更新

---
+++ testvuecli3/src/components/son.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>SON</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  beforeCreate() {
+    console.log("beforeCreateS");
+  },
+  created() {
+    console.log("createdS");
+  },
+  beforeMount() {
+    console.log("beforeMountS");
+  },
+  mounted() {
+    console.log("mountedS");
+  },
+  mounted() {
+    console.log("mountedS");
+  },
+};
+</script>
+
+<style scoped lang="scss">
+</style>
